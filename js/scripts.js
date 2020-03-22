ddc802001e50ca8e7c7b1235922d588c7fe698db
mamouflon <61464412+mamouflon@users.noreply.github.com>
2020-03-22 11:40:48 +0100

    Added several pokemon to the project with name, height, types and abilities

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,53 @@ var repository= [
     PokemonHeight: 2.04,
     PokemonTypes: ['Grass', 'Poison'],
     PokemonAbilities: ['Overgrow'],
+  },
+  {
+    PokemonName: 'Ivysaur',
+    PokemonHeight: 3.03,
+    PokemonTypes: ['Grass', 'Poison'],
+    PokemonAbilities: ['Overgrow'],
+  },
+  {
+    PokemonName: 'Venusaur',
+    PokemonHeight: 6.07,
+    PokemonTypes: ['Grass', 'Poison'],
+    PokemonAbilities: ['Overgrow'],
+  },
+  {
+    PokemonName: 'Charmander',
+    PokemonHeight: 2.00,
+    PokemonTypes: ['Fire'],
+    PokemonAbilities: ['Blaze'],
+  },
+  {
+    PokemonName: 'Charmeleon',
+    PokemonHeight: 3.07,
+    PokemonTypes: ['Fire'],
+    PokemonAbilities: ['Blaze'],
+  },
+  {
+    PokemonName: 'Charizard',
+    PokemonHeight: 5.07,
+    PokemonTypes: ['Fire', 'Flying'],
+    PokemonAbilities: ['Blaze'],
+  },
+  {
+    PokemonName: 'Squirtle',
+    PokemonHeight: 1.08,
+    PokemonTypes: ['Water'],
+    PokemonAbilities: ['Torrent'],
+  },
+  {
+    PokemonName: 'Wartortle',
+    PokemonHeight: 3.03,
+    PokemonTypes: ['Water'],
+    PokemonAbilities: ['Torrent'],
+  },
+  {
+    PokemonName: 'Blastoise',
+    PokemonHeight: 5.03,
+    PokemonTypes: ['Water'],
+    PokemonAbilities: ['Torrent'],
   }
 ]
